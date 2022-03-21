7d655826e0ccf3ecec67952715b1a78b7f29a09f
Mateus Benites Dias <imattdias@gmail.com>
2022-03-21 17:58:10 -0300

    CTD - Front End III - Aula 07 - Exercício Sync

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function Button(props){
+    return(
+        <button
+        onClick={props.onClick} 
+        className={props.texto === "Adicionar" ? "button" : "button2"}
+        >{props.texto}</button>
+    );
+}
